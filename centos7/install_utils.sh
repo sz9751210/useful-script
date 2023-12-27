718b735432ad469617fcdde66bed2234b16404bf
alan_wang <sz9751210@gmail.com>
2023-12-27 03:57:29 +0000

    add make command

--- centos7/install_utils.sh
+++ centos7/install_utils.sh
@@ -12,8 +12,11 @@ sudo yum install wget -y
 # Install telnet
 sudo yum install telnet -y
 
+sudo yum install make -y
+
 # Check the installations
 echo "Installed versions:"
 curl --version | head -1
 wget --version | head -1
 yum list installed | grep telnet
+make --version | head -1
